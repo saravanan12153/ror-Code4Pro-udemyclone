7661222a8fd692bdb8ea8984626c4311e07b7138
Sulman Baig <sulman1990@hotmail.com>
2015-05-11 11:58:36 +0200

    Added gravatar and toastr

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'materialize-sass'
 
 gem 'devise', '~> 3.4.1'
+gem 'toastr-rails'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require materialize-sprockets
 //= require jquery_ujs
+//= require materialize-sprockets
+//= require toastr
 //= require turbolinks
 //= require_tree .
